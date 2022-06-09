52d86c4704a15e1da8a570f9eb6749c49f85a1e6
Alexandre Neves <alexandre.neves@garland.pt>
2022-06-09 12:11:42 +0100

    Navbar & FontAwesome

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Nav />
+  <main class="container">
+    <i class="fa-duotone fa-handshake bigIcon"></i>
+    <h1>Welcome to Handshake CRM</h1>
+  </main>
 </template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+
+.bigIcon {
+  font-size: 150px;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    meta: {
+        link: [
+          {
+            rel: "stylesheet",
+            href: "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+          },
+        ],
+    },
+    css: [
+      '@/assets/font-awesome/css/all.css',
+      '@/assets/css/global.css'
+    ]
 })
